c6391f303413b78d591357acbd8c965e0d2405a2
Intisarhaque <arsenal.15@hotmail.co.uk>
2020-02-17 19:11:32 +0000

    multiple files

--- test_code/test_code.py
+++ test_code/test_code.py
@@ -9,7 +9,12 @@ import csv
 from math import sqrt
 from dateutil import parser
 from datetime import datetime
+import glob
 
+txtfiles = []
+for file in glob.glob("*.csv"):
+    txtfiles.append(file)
+print(txtfiles)
 METRICRESULTS = 2
 
 
@@ -24,9 +29,7 @@ for item in r:
         # item.append("0")
     item.append("candlestick")
     item.append("0")
-    all.append(item)
-
-        
+    all.append(item)        
 with open('output.csv', 'w') as csvoutput:
      writer = csv.writer(csvoutput, lineterminator='\n')
      writer.writerows(all)
@@ -56,36 +59,18 @@ with open('output.csv', 'w') as csvoutput:
     writer = csv.writer(csvoutput, lineterminator='\n')
     writer.writerows(all)
     
-xaxis = result['datetime']
-yaxismu = result['close']
-plt.figure(0)
-plt.plot(xaxis, yaxismu)
-plt.xlabel('Time')
-plt.ylabel('Close')
-plt.title('Mu against Frequency to determine device stability')
-plt.show()
+# xaxis = result['datetime']
+# yaxismu = result['close']
+# plt.figure(0)
+# plt.plot(xaxis, yaxismu)
+# plt.xlabel('Time')
+# plt.ylabel('Close')
+# plt.title('Mu against Frequency to determine device stability')
+# plt.show()
 
 
 
-# date = "02.01.2018 14:30:00.000 GMT-0000"
-# date = date[0:-13]
-# date = datetime.strptime(date, '%d.%m.%Y %H:%M:%S')
 
-# v = open('AMD.USUSD_Candlestick_1_s_BID_02.01.2018-02.01.2018.csv')
-# w = open('AMD.USUSD_Candlestick_1_s_BID_02.01.2018-02.01.2018.csv', 'w')
-# r = csv.reader(v)
-# all = []
-# row0 = next(r)
-# for item in r:
-    # for i in range(0,METRICRESULTS):
-        # item.append("0")
-    # item.append("0")
-    # all.append(row)
-    #print(item)
-    
-    
-# print(result[0]['high'])
-# print(date.time())
 
 
 
@@ -101,3 +86,4 @@ plt.show()
 #https://stackoverflow.com/questions/20387359/regex-to-delete-whitespace-in-csv-file-with-quotes-to-separate-text
 #https://www.machinelearningplus.com/time-series/time-series-analysis-python/
 #https://www.quandl.com/data/EOD/AMD-Advanced-Micro-Devices-Inc-AMD-Stock-Prices-Dividends-and-Splits
+#https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
